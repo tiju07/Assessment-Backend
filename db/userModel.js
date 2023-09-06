40f0c157502c15f2a638b648e6b3e6151f35e3b3
Tiju <74893674+tiju07@users.noreply.github.com>
2023-09-06 13:03:48 +0530

    Update userModel.js

--- db/userModel.js
+++ db/userModel.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username!"],
+        unique: [true, "Username already exists!"],
+    },
     email: {
         type: String,
         required: [true, "Please provide an Email!"],
         unique: [true, "Email Exist"],
     },
-
+    dob: {
+        type: Date,
+        required: [true, "Please provide your date of birth!"],
+    },
     password: {
         type: String,
         required: [true, "Please provide a password!"],
